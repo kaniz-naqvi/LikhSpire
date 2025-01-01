06cde1e26add16fad8ac7f84ead22f4607652bf4
Mehak <mehak313naqvi@gmail.com>
2025-01-01 20:26:22 +0500

    Updated files after editing

--- script.js
+++ script.js
@@ -1,40 +1,49 @@
 const projects = [
-    {
-      day: 1,
-      title: "Simple counter App",
-      description:
-        "A simple counter app built with React, demonstrating my first use of the useState() hook for managing state in a functional component. It allows users to increment or decrement the count.",
-      projectLink: "https://s4dzs3.csb.app/",
-      codeLink:
-        "https://codesandbox.io/p/sandbox/counter-app-s4dzs3?file=%2Fsrc%2Findex.js&workspaceId=ws_v5yDKkVuiyaKEjHf863pL",
-    },
-    {
-      day: 2,
-      title: "LiveClock",
-      description:
-        "Real-time clock that updates every second to show the current time. No need to refresh, just click to get the latest time instantly.",
-      projectLink: "https://ls9pzg.csb.app/",
-      codeLink:
-        "https://codesandbox.io/p/sandbox/liveclock-ls9pzg?file=%2Fsrc%2Fcomponents%2FApp.jsx%3A5%2C25",
-    },
-    {
-      day: 3,
-      title: "TechTrends 2025",
-      description:
-        "A React app that showcases the latest technology trends to watch in 2024, with an interactive list where users can click on each trend to view detailed information. This project uses React, the useState hook, JSX for rendering components, and CSS for styling.",
-      projectLink: "https://8tdvwl.csb.app/",
-      codeLink: "https://codesandbox.io/p/sandbox/techtrends-2025-8tdvwl?file=%2Fpublic%2Fstyles.css%3A9%2C15",
-    },
-  ];
-  
-  const projectsContainer = document.getElementById("projects");
-  
-  // Render Projects Dynamically
-  projects.forEach((project) => {
-    const projectCard = document.createElement("div");
-    projectCard.classList.add("project-card");
-  
-    projectCard.innerHTML = `
+  {
+    day: 1,
+    title: "Simple counter App",
+    description:
+      "A simple counter app built with React, demonstrating my first use of the useState() hook for managing state in a functional component. It allows users to increment or decrement the count.",
+    projectLink: "https://s4dzs3.csb.app/",
+    codeLink:
+      "https://codesandbox.io/p/sandbox/counter-app-s4dzs3?file=%2Fsrc%2Findex.js&workspaceId=ws_v5yDKkVuiyaKEjHf863pL",
+  },
+  {
+    day: 2,
+    title: "LiveClock",
+    description:
+      "Real-time clock that updates every second to show the current time. No need to refresh, just click to get the latest time instantly.",
+    projectLink: "https://ls9pzg.csb.app/",
+    codeLink:
+      "https://codesandbox.io/p/sandbox/liveclock-ls9pzg?file=%2Fsrc%2Fcomponents%2FApp.jsx%3A5%2C25",
+  },
+  {
+    day: 3,
+    title: "TechTrends 2025",
+    description:
+      "A React app that showcases the latest technology trends to watch in 2024, with an interactive list where users can click on each trend to view detailed information. This project uses React, the useState hook, JSX for rendering components, and CSS for styling.",
+    projectLink: "https://8tdvwl.csb.app/",
+    codeLink:
+      "https://codesandbox.io/p/sandbox/techtrends-2025-8tdvwl?file=%2Fpublic%2Fstyles.css%3A9%2C15",
+  },
+  {
+    day: 4,
+    title: "To-do List App",
+    description:
+      "A simple React app that allows users to add, view, and delete tasks from their to-do list. The app includes the ability to mark tasks with a checkbox and shows the date when each task was added. This project is built using React, useState hook, and CSS for styling.",
+    projectLink: "https://kaniz-naqvi.github.io/To-do-App/",
+    codeLink: "https://github.com/kaniz-naqvi/To-do-App",
+  },
+];
+
+const projectsContainer = document.getElementById("projects");
+
+// Render Projects Dynamically
+projects.forEach((project) => {
+  const projectCard = document.createElement("div");
+  projectCard.classList.add("project-card");
+
+  projectCard.innerHTML = `
       <h1>Day ${project.day} task</h1>
         <h2>${project.title}</h2>
         <p>${project.description}</p>
@@ -43,7 +52,6 @@ const projects = [
           <a href="${project.codeLink}" target="_blank" class="btn secondary">View Code</a>
         </div>
       `;
-  
-    projectsContainer.appendChild(projectCard);
-  });
-  
+
+  projectsContainer.appendChild(projectCard);
+});
